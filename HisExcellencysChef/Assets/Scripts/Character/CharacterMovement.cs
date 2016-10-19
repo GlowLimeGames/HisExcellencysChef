a2570132629cb334fc320d6a93705548cda8eb16
Paul Calande <paul.luap09@gmail.com>
2016-10-19 17:54:52 -0400

    - Characters are now outlined when they are selected. - Added some (third-party) shaders and scripts for the glowing effect. - Added a new layer for the glowing effect. - Added new components to the camera and characters for the glowing effect. - Added a CharacterProperties script that contains some properties of the characters: isCook, atStation, and heldDish. - All clicking is now handled through the ClickHandler GameObject and script. - Modified the CharacterMovement script to reflect the new ClickHandler. - Characters that can be controlled now have the "PlayerChar" tag. - The Cook is now selected by default. - The controllable characters are now based on a prefab. - The ranges are now based on a prefab. - Fixed some layer order issues in the scene. The cauldron is still a bit iffy, but we can worry more about that as the movement engine is changed.

---
+++ HisExcellencysChef/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private bool isMoving = false;
+
+	public void Move(Vector2 position){
+		if (!isMoving) {
+			StopAllCoroutines ();
+			StartCoroutine ("MoveTo", position);
+		}
+	}
+
+	IEnumerator MoveTo (Vector2 position) {
+		isMoving = true;
+		while (Vector2.Distance(transform.position, position) > 0.1f) {
+			transform.position = Vector2.MoveTowards (transform.position, position, speed * Time.deltaTime);
+			yield return new WaitForFixedUpdate();
+		}
+		isMoving = false;
+	}
+
+	
+}
