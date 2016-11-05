e79b4602d43a34790c943a40a7ab2d896b706d0d
Jowell-E <jjoelesquilin@gmail.com>
2016-11-05 14:24:56 -0400

    Close enough Prototype
    -Pantry gives ingredient to character, each character can hold an
    ingredient and move with it to a station, place it on the station where
    it will "cook" for it's set time then it may be picked up and moved
    again.

--- HisExcellencysChef/Assets/Scripts/Ingredients.cs
+++ HisExcellencysChef/Assets/Scripts/Ingredients.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Ingredients : MonoBehaviour {
 	public int numI= 4; //four ingredients that we have listed so far
 
-    public string name;
+    public string foodName;
 	public float cookTime;
     public Vector2 flavor;
 
@@ -14,167 +14,171 @@ public class Ingredients : MonoBehaviour {
 	void Start () {
 	}
 
-    public void cookSelf(string tag)
+    public void cookSelf(string tag , GameObject character)
     {
-        if (tag == "cut")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-                
-            }
-            if (name== "")
-            {
-
-            }
-            if (name== "Water")
-            {
-
-            }
-        }
-
-        //add more functions for the various tags
-        
-        if (tag == "fry")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
-        if (tag == "bake")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
-        if (tag == "boil")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
-        if (tag == "sliver")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
-        if (tag == "pound")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
-        if (tag == "press")
-        {
-            if (name == "Almond")
-            {
-                Countdown("newFoodName");
-            }
-            if (name == "Saffron")
-            {
-
-            }
-            if (name == "")
-            {
-
-            }
-            if (name == "Water")
-            {
-
-            }
-        }
+		Countdown ("", character);
+//        if (tag == "cut")
+//        {
+//            if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//                
+//            }
+//			if (foodName== "")
+//            {
+//
+//            }
+//			if (foodName== "Water")
+//            {
+//
+//            }
+//        }
+//
+//        //add more functions for the various tags
+//        
+//        if (tag == "fry")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
+//        if (tag == "bake")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
+//        if (tag == "boil")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
+//        if (tag == "sliver")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
+//        if (tag == "pound")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
+//        if (tag == "press")
+//        {
+//			if (foodName == "Almond")
+//            {
+//                Countdown("newFoodName");
+//            }
+//			if (foodName == "Saffron")
+//            {
+//
+//            }
+//			if (foodName == "")
+//            {
+//
+//            }
+//			if (foodName == "Water")
+//            {
+//
+//            }
+//        }
     }
 
-    void Countdown(string newFood)
+    void Countdown(string newFood, GameObject character)
     {
         if (!isCooking)
         {
-            StartCoroutine("CookTimeStartGo", newFood);
+            StartCoroutine("CookTimeStartGo", character);
         }
     }
 
-    IEnumerator CookTimeStartGo(string newFood)
+    IEnumerator CookTimeStartGo(GameObject character)
     {
+		character.GetComponent<CharacterMovement> ().isCooking = true;
+		isCooking = true;
         float percentComplete = 0f;
-        while (percentComplete != 1)
+        while (percentComplete <= 1)
         {
-            percentComplete += Time.deltaTime;
+            percentComplete += Time.deltaTime / cookTime;
 
             yield return null;
         }
-
         Debug.Log("Ding");
+		character.GetComponent<CharacterMovement> ().isCooking = false;
         isCooking = false;
-        //Instantiate(newFood);
+		GetComponent<SpriteRenderer> ().color = new Color32 (150, 150, 150, 255);
+		//Instantiate(newFood);
         //Destroy(thisFood)
 
     }
