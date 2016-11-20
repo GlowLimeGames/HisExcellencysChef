d3ef66fc7d9cf70d9bc216cd8f12ac43a367dd88
Isaiah Mann <contact@isaiahmann.com>
2016-11-20 17:17:50 -0500

    Audio Audio files and default scripts

---
+++ HisExcellencysChef/Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: 
+ */
+
+using UnityEngine;
+
+public class GameController : SingletonController<GameController> {
+	[SerializeField]
+	bool playMusicOnStart = true;
+
+	protected override void FetchReferences () {
+		base.FetchReferences ();
+		if (playMusicOnStart) {
+			playMusic();
+		}
+	}
+
+	void playMusic () {
+		EventController.Event(Event.GAME_MUSIC);
+	}
+}
---
+++ HisExcellencysChef/Assets/Scripts/DefaultUnityScripts/Event/Event.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Event {
+	public const string WIN = "Win";
+	public const string START_MUSIC = "PlayStartMusic";
+	public const string GAME_MUSIC = "PlayGameMusic";
+}
