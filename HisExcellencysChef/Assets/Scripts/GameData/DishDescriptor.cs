0ef87b6bddf9b6cf65e51e24ea70b08275f8c7ab
Isaiah Mann <contact@isaiahmann.com>
2016-11-20 22:06:11 -0500

    Parsing dishes JSON

---
+++ HisExcellencysChef/Assets/Scripts/GameData/DishDescriptor.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: 
+ */
+
+[System.Serializable]
+public class DishDescriptor : HECData {
+	public string Ingredient;
+	public string Chop;
+	public string Sliver;
+	public string Mince;
+	public string Pound;
+	public string Fry;
+	public string Seethe;
+	public string Bake;
+	public string Roast;
+	public string Boil;
+}
+
+[System.Serializable]
+public class DishDescriptorList : HECDataList<DishDescriptor> {}
