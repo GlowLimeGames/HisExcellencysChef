c9fb0cb83956420c17abd6894fe4aaf0e5783878
Paul Calande <paul.luap09@gmail.com>
2016-12-05 04:34:27 -0500

    Worked for a couple hours, but the flavor display is currently bugged because SetReferences() isn't running in Ingredients.cs?

--- HisExcellencysChef/Assets/Scripts/Inventory.cs
+++ HisExcellencysChef/Assets/Scripts/Inventory.cs
@@ -1,110 +1,127 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+ * Author(s): Paul Calande
+ * Description: Inventory class for the pantry.
+ */
 
-public class Inventory : MonoBehaviour {
+using UnityEngine;
+using System.Collections.Generic;
 
-    public int inventorySize = 10; // Number of elements in the inventory.
-
-    public GameObject[] inventory; // The inventory is an array of GameObjects.
-
-	void Start () {
-        inventory = new GameObject[inventorySize]; // Initialize the inventory.
-        for (int i=0; i<inventorySize; ++i)
-        {
-            inventory[i] = null; // Set every element in the inventory to null. Use null for empty slots.
-        }
-	}
-
-    public int GetFirstEmpty() // Returns the index of the first empty element in the inventory. Returns -1 if the inventory is full.
+public class Inventory : MonoBehaviour
+{
+    // Serializing the inventory node allows us to access it in the Inspector!
+    [System.Serializable]
+    public struct InventoryNode
     {
-        for (int i=0; i<inventorySize; ++i)
+        public GameObject obj;
+        public int quantity;
+
+        public InventoryNode(GameObject o, int q)
         {
-            if (inventory[i] == null)
+            //Debug.Log("Inventory.cs: InventoryNode 2-argument constructor");
+            obj = o;
+            quantity = q;
+        }
+    }
+    // The inventory uses a dictionary with string keys, each of which
+    // maps onto an inventory node with a GameObject and a quantity.
+    public Dictionary<string, InventoryNode> dict = new Dictionary<string, InventoryNode>();
+
+    // Adds a number of ingredients to the inventory.
+    // Use -2 for an infinite quantity of ingredients.
+    public void Add(GameObject obj, int quantity)
+    {
+        // Try to retrieve the Ingredients component from the GameObject.
+        Ingredients ingred = obj.GetComponent<Ingredients>();
+        // If the GameObject does not have an Ingredients component...
+        if (ingred == null)
+        {
+            // We are so screwed.
+            Debug.LogError("Inventory.cs: Tried to add a GameObject that wasn't an ingredient to dictionary!");
+            return;
+        }
+        // Get the ingredient name.
+        string name = GetIngredientName(obj);
+        //Debug.Log("Inventory.cs: component is: " + ingred + ", and ingredient name is: " + name);
+        InventoryNode val;
+        // If the dictionary already contains this type of ingredient...
+        if (dict.TryGetValue(name, out val))
+        {
+            // Add to the quantity.
+            val.quantity += quantity;
+        }
+        // Otherwise, if this ingredient isn't in the dictionary...
+        else
+        {
+            // Add an new entry to the dictionary.
+            dict.Add(name, new InventoryNode(obj, quantity));
+        }
+    }
+
+    // Gets the quantity of an ingredient. Returns -1 if nothing was found.
+    public int GetQuantity(GameObject obj)
+    {
+        Ingredients ingred = obj.GetComponent<Ingredients>();
+        if (ingred == null)
+        {
+            Debug.LogError("Inventory.cs: Tried to access dictionary with non-ingredient GameObject!");
+            return -1;
+        }
+        string name = GetIngredientName(obj);
+        InventoryNode val;
+        // If the ingredient is in the dictionary...
+        if (dict.TryGetValue(name, out val))
+        {
+            // Return the quantity of that ingredient.
+            return val.quantity;
+        }
+        // If the ingredient isn't there...
+        else
+        {
+            return -1;
+        }
+    }
+
+    // Removes one ingredient from the inventory.
+    public void RemoveOne(GameObject obj)
+    {
+        Ingredients ingred = obj.GetComponent<Ingredients>();
+        if (ingred == null)
+        {
+            Debug.LogError("Inventory.cs: Tried to access dictionary with non-ingredient GameObject!");
+            return;
+        }
+        string name = GetIngredientName(obj);
+        InventoryNode val;
+        if (dict.TryGetValue(name, out val))
+        {
+            if (val.quantity == -2)
             {
-                return i;
+                // A quantity of -2 means infinite.
+                // No point in continuing this function from here.
+                return;
             }
-        }
-        return -1;
-    }
-
-    public bool IsFull() // Returns whether the inventory is full.
-    {
-        return GetFirstEmpty() == -1;
-    }
-
-    public bool IsElementEmpty(int index) // Returns whether an element in the inventory is empty (i.e. whether the element doesn't have an item).
-    {
-        return inventory[index] == null;
-    }
-
-    public GameObject TakeItem(int index) // Returns an item from a certain index and removes that item from the inventory.
-    {
-        if (index >= inventorySize || index < 0)
-        {
-            Debug.Log("<color=red>"+name+" INVENTORY WARNING: Tried to take item from invalid index " + index + "</color>");
-            return null;
-        }
-        if (inventory[index] == null)
-        {
-            return null;
+            // Reduce quantity by one.
+            --val.quantity;
+            if (val.quantity == 0)
+            {
+                // We're out of this ingredient... Remove it from the dictionary.
+                dict.Remove(name);
+            }
         }
         else
         {
-            GameObject item = inventory[index];
-            inventory[index] = null;
-            return item;
+            Debug.LogError("Inventory.cs: Tried to remove an ingredient that wasn't in the inventory!");
         }
     }
 
-    public bool PutItem(int index, GameObject item) // Puts an item into the inventory if the index is empty. Returns true if successful.
+    // Returns the length of the inventory/dictionary.
+    public int Length()
     {
-        if (index >= inventorySize || index < 0)
-        {
-            Debug.Log("<color=red>" + name + " INVENTORY WARNING: Tried to put item into invalid index " + index + "</color>");
-            return false;
-        }
-        if (IsElementEmpty(index))
-        {
-            inventory[index] = item;
-            return true;
-        }
-        return false;
+        return dict.Count;
     }
 
-    public bool PutItem(GameObject item) // Dumps an item into the inventory at the first available index.
+    private string GetIngredientName(GameObject obj)
     {
-        int firstEmpty = GetFirstEmpty(); // Get the first empty element.
-        if (firstEmpty == -1) // If the inventory is full...
-        {
-            return false; // You fail.
-        }
-        else // Otherwise...
-        {
-            inventory[firstEmpty] = item; // Put the item into the inventory.
-            return true; // You're a winner at life, among other things.
-        }
-    }
-
-    public bool SetItem(int index, GameObject item) // Changes an item in the inventory into a different item. Returns true if successful.
-    {
-        if (index >= inventorySize || index < 0)
-        {
-            Debug.Log("<color=red>" + name + " INVENTORY WARNING: Tried to set item in invalid index " + index + "</color>");
-            return false;
-        }
-        inventory[index] = item;
-        return true;
-    }
-
-    public GameObject SwapItem(int index, GameObject item) // Replaces an item in the inventory and returns the item that was there previously.
-    {
-        if (index >= inventorySize || index < 0)
-        {
-            Debug.Log("<color=red>" + name + " INVENTORY WARNING: Tried to swap item at invalid index " + index + "</color>");
-            return null;
-        }
-        GameObject oldItem = inventory[index];
-        inventory[index] = item;
-        return oldItem;
+        return obj.name;
     }
 }
--- HisExcellencysChef/Assets/Scripts/InventoryButton.cs
+++ HisExcellencysChef/Assets/Scripts/InventoryButton.cs
@@ -1,27 +1,68 @@
-﻿using UnityEngine;
+﻿/*
+ * Author(s): Paul Calande, Joel Esquilin
+ * Description: Inventory button class.
+ */
+
+using UnityEngine;
 using System.Collections;
 
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class InventoryButton : MonoBehaviour
 {
-    public int itemNum;
-    public Inventory inv;
-    public GameObject character;
-	public GameObject[] foods = new GameObject[43];
+    private GameObject item;
+    private InventoryUI invui;
+
+    private void Start()
+    {
+        //Debug.Log("InventoryButton.cs: I'm real.");
+        invui = transform.parent.gameObject.GetComponent<InventoryUI>();
+    }
 
     public void ButtonClick()
     {
-		character = GameObject.Find ("ClickHandler").GetComponent<ClickHandler> ().currentCharacter;
+        // Get the currently-selected character.
+		GameObject character = GameObject.Find ("ClickHandler").GetComponent<ClickHandler> ().currentCharacter;
+        // Then, get their CharacterProperties component.
         CharacterProperties cp = character.GetComponent<CharacterProperties>();
-		if (cp.heldDish == null) {
-			GameObject food = (GameObject)Instantiate (foods [itemNum], cp.transform.position + new Vector3 (0, 1.5f, 0), Quaternion.identity);
+        // If the character isn't holding anything...
+		if (cp.heldDish == null)
+        {
+            // Instantiate the food above the player's head.
+			GameObject food = (GameObject)Instantiate (item, cp.transform.position + new Vector3 (0, 1.5f, 0), Quaternion.identity);
 			food.GetComponent<Ingredients>().primaryIngredientName = food.name.Replace ("(Clone)", "").Trim();
+            // Make the player have this food as the held dish.
 			cp.heldDish = food;
+            // Set the player as the food's parent.
 			food.transform.parent = cp.transform;
-			transform.parent.gameObject.SetActive (false); // Disable the inventory window.
-		} else {
-			transform.parent.gameObject.SetActive (false);
+            // Disable the inventory window.
+            invui.SetEnabled (false);
+            // Remove the item from the inventory.
+            // [later]
 		}
+        // If the character IS holding something...
+        else
+        {
+			invui.SetEnabled (false);
+		}
+    }
+
+    // Sets the ingredient GameObject to which the inventory button refers.
+    public void SetGameObject(GameObject obj)
+    {
+        item = obj;
+    }
+
+    public void MouseOver()
+    {
+        //Debug.Log("MOUSE OVER!");
+        // Get the text component of the inventory data window's child, which is a text object.
+        Text txt = invui.inventoryData.transform.GetChild(0).GetComponent<Text>();
+        // Get the ingredient data of the button's food.
+        Ingredients ingred = item.GetComponent<Ingredients>();
+        string name = item.name;
+        string moistheat = ingred.HeatAndMoistureToString();
+        // Change the text.
+        txt.text = name + '\n' + moistheat;
     }
 }
