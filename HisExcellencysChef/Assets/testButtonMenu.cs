b52b74b3b235058f007760f03aa1ed0fad88061c
wsattanuparp <willsattanuparp@gmail.com>
2017-02-17 18:13:14 -0500

    First-Commit
    yeah

---
+++ HisExcellencysChef/Assets/testButtonMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class testButtonMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
